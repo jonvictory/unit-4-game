8890b0f7ac25b8fa611184b063fe35d4019412d4
jonhuynh <jonvictory@gmail.com>
2019-09-08 23:16:56 -0600

    added character objects and some possible fighting divs

--- assets/js/game2.js
+++ assets/js/game2.js
@@ -1,5 +1,46 @@
 window.onload = function () {
-    
+var heroOne = {
+    hp: 800,
+    ap: 6,
+    cap: 2
+}
+
+var heroTwo = {
+    hp: 600,
+    ap: 8,
+    cap: 4
+}
+
+var heroThree = {
+    hp: 1000,
+    ap: 4,
+    cap: 1
+}
+
+var villainOne = {
+    hp: 200,
+    ap: 3,
+    cap: 3
+}
+
+var villainTwo = {
+    hp: 100,
+    ap: 4,
+    cap: 4
+}
+
+var villainThree = {
+    hp: 300,
+    ap: 2,
+    cap: 2
+}
+
+var villainFour = {
+    hp: 50,
+    ap: 5,
+    cap: 5
+}
+
 }
 
 $(".startScreen").click(function(){
@@ -27,4 +68,8 @@ $(".charSelected").click(function(){
     $(".charSelect").fadeOut("slow");
     $(".charSelected").fadeOut("slow");
     $(".bg").fadeIn("slow");
+})
+
+$(".mapArea").click(function() {
+    $(".fight").fadeIn("slow");
 })
